19a03570a027f852f02ff11b7741de4a526acd76
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-11-16 13:52:19 -0600

    finished server.js and added package.json

--- Develop/server.js
+++ Develop/server.js
@@ -6,7 +6,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use()
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 //start the server
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
