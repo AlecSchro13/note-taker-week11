a354c66d7bea8d168b68826a9a2279491d041f8e
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-11-16 11:27:39 -0600

    connecting routes and html pages

--- Develop/server.js
+++ Develop/server.js
@@ -4,3 +4,9 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use()
+
+//start the server
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
