16c5e00acbcc0d6a1ae91462c2674bfa87a1b771
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-11-17 10:39:08 -0600

    added route to notes.html page

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const router = require('express').Router();
 
+// '/notes' is used to get the parent folder to respond
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
 
 
 module.exports = router;
