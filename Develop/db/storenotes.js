88bf0548eed13bfd993178e66bbd268ad82b1727
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-11-17 11:14:15 -0600

    started working in the apiroutes.js file

--- Develop/db/storenotes.js
+++ Develop/db/storenotes.js
@@ -3,15 +3,9 @@ const fs = require('fs');
 
 // this is to create unique ids
 const uuidv1 = require('uuid/v1');
-//allow user to read notes and stringifies them
 
-class StoreNote {
-    read() {
-        return readFileAsync('db/db.json')
-    }
-}
 
-// adds unique id
 
-//exports new notes that are stored
-module.exports = new StoreNote();
+
+class StoreNotes {}
+
