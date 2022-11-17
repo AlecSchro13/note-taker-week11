f35108cfbd3cc5f22a141885f4a0a1545680c767
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-11-17 10:26:51 -0600

    changed server.js to where you can click on liink in the app.listen to look at notetaker

--- Develop/db/storenotes.js
+++ Develop/db/storenotes.js
@@ -2,7 +2,7 @@ const util = require('util');
 const fs = require('fs');
 
 // this is to create unique ids
-
+const uuidv1 = require('uuid/v1');
 //allow user to read notes and stringifies them
 
 class StoreNote {
