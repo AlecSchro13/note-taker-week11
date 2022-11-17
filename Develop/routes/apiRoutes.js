a675bd90b955ce15b352aa12a4f7c2368fc4614a
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-11-17 12:56:14 -0600

    making router.gets in apiroutes.js file

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,21 +1,30 @@
+const express = require("express");
 const router = require('express').Router();
+const {v4: uuidv4} = require("uuid");
+const fs = require("fs")
 const store = require('../utils/fsUtils');
 
 router.get('/notes', (req, res) => {
     store
-      .getNotes()
-      .then((notes) => {
-        return res.json(notes);
-      })
-      .catch((err) => res.status(500).json(err));
+    
+      .readFromFile('./db/db.json')
+      .then((data) => res.json(JSON.parse(data)));
+})  
+  router.get('/notes:id', (req, res) => {
+     const Id = req.params.id; 
+
+     const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+
+     for (note of notes) {
+        if(Id === note.id) {
+            return res.json(note);
+        }
+     }
+
+     return res.json("note not found");
   });
-  
-  router.post('/notes', (req, res) => {
-    store
-      .addNote(req.body)
-      .then((note) => res.json(note))
-      .catch((err) => res.status(500).json(err));
-  });
+
+  router.post('/notes')
 
 
 module.exports = router;
